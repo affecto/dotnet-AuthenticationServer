ec6bf1677b4bae884813bdad61b9fae75626e368
Ville Henriksson <teettari@hotmail.com>
2016-04-21 15:08:42 +0300

    Use Kentor AuthServices with IdentityServer3 saml authentication.

---
+++ Source/AuthenticationServer.IdentityManagement/ClaimsAuthenticationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace Affecto.AuthenticationServer.IdentityManagement
+{
+    public class ClaimsAuthenticationManager : System.Security.Claims.ClaimsAuthenticationManager
+    {
+        /// <summary>
+        /// When overridden in a derived class, returns a <see cref="T:System.Security.Claims.ClaimsPrincipal"/> object consistent with the requirements of the RP application. The default implementation does not modify the incoming <see cref="T:System.Security.Claims.ClaimsPrincipal"/>.
+        /// </summary>
+        /// <returns>
+        /// A claims principal that contains any modifications necessary for the RP application. The default implementation returns the incoming claims principal unmodified.
+        /// </returns>
+        /// <param name="resourceName">The address of the resource that is being requested.</param><param name="incomingPrincipal">The claims principal that represents the authenticated user that is attempting to access the resource.</param>
+        public override ClaimsPrincipal Authenticate(string resourceName, ClaimsPrincipal incomingPrincipal)
+        {
+            return base.Authenticate(resourceName, incomingPrincipal);
+        }
+    }
+}
