dc62e2e9bc56ef230d6d34323683ed8090dbe212
Ville Henriksson <teettari@hotmail.com>
2016-04-27 14:12:57 +0300

    Do not show consent screen.

--- Source/AuthenticationServer/Configuration/ExtensionMethods.cs
+++ Source/AuthenticationServer/Configuration/ExtensionMethods.cs
@@ -29,7 +29,8 @@ namespace Affecto.AuthenticationServer.Configuration
                 AccessTokenLifetime = (int) client.AccessTokenLifetime.TotalSeconds,
                 Enabled = client.Enabled,
                 AllowedScopes = client.AllowedScopes.ToList(),
-                RedirectUris = client.RedirectUri != null ? new List<string> { client.RedirectUri.OriginalString } : null
+                RedirectUris = client.RedirectUri != null ? new List<string> { client.RedirectUri.OriginalString } : null,
+                RequireConsent = false
             }).ToList();
         }
     }
