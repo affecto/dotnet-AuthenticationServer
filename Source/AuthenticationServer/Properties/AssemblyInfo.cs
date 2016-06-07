a3452c5a2cc9241c18663aaadd714abdc724e79a
sjogrtom <sjogrtom@W10X64-SJOGRTOM>
2016-06-07 13:58:24 +0300

    Raised version number to 2.2.0

--- Source/AuthenticationServer/Properties/AssemblyInfo.cs
+++ Source/AuthenticationServer/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("Affecto Authentication Server")]
 [assembly: AssemblyCompany("Affecto")]
 
-[assembly: AssemblyVersion("2.1.2.0")]
-[assembly: AssemblyFileVersion("2.1.2.0")]
-[assembly: AssemblyInformationalVersion("2.1.2")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0")]
 
 [assembly: InternalsVisibleTo("Affecto.AuthenticationServer.Tests")]
 [assembly: InternalsVisibleTo("Affecto.AuthenticationServer.Configuration.Tests")]
