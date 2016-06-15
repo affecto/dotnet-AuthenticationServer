8435f9bfa529459bc6e0eea7ecc4b4355ac59f8b
johannesvaltonen <johannes.valtonen@affecto.com>
2016-06-15 17:56:32 +0300

    Copy test config files to a separate subfolder.

--- Source/AuthenticationServer.Configuration.Tests/ConfigurationTestsBase.cs
+++ Source/AuthenticationServer.Configuration.Tests/ConfigurationTestsBase.cs
@@ -1,14 +1,17 @@
 using System;
+using System.IO;
 using Affecto.Testing.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Affecto.AuthenticationServer.Configuration.Tests
 {
-    [DeploymentItem("ConfigFiles")]
+    [DeploymentItem("ConfigFiles", "AuthenticationServer.Configuration.Tests")]
     public abstract class ConfigurationTestsBase
     {
-        private readonly ConfigSectionReader configSectionReader = new ConfigSectionReader(Environment.CurrentDirectory);
         protected IAuthenticationServerConfiguration authenticationServerConfiguration;
+
+        private readonly ConfigSectionReader configSectionReader =
+            new ConfigSectionReader(Path.Combine(Environment.CurrentDirectory, "AuthenticationServer.Configuration.Tests"));
 
         protected void SetupAuthenticationServerConfiguration(string configFileName)
         {
--- Source/AuthenticationServer.Plugins.Infrastructure.Tests/Configuration/ConfigurationTestsBase.cs
+++ Source/AuthenticationServer.Plugins.Infrastructure.Tests/Configuration/ConfigurationTestsBase.cs
@@ -1,19 +1,24 @@
 using System;
+using System.IO;
 using Affecto.AuthenticationServer.Plugins.Infrastructure.Configuration;
 using Affecto.Testing.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AuthenticationServer.Plugins.Infrastructure.Tests.Configuration
 {
-    [DeploymentItem("Configuration\\ConfigFiles")]
+    [DeploymentItem("Configuration\\ConfigFiles", "Plugins.Infrastructure.Tests.Configuration")]
     public abstract class ConfigurationTestsBase
     {
-        private readonly ConfigSectionReader configSectionReader = new ConfigSectionReader(Environment.CurrentDirectory);
         protected IFederatedAuthenticationConfiguration federatedAuthenticationConfiguration;
+
+        private readonly ConfigSectionReader configSectionReader =
+            new ConfigSectionReader(Path.Combine(Environment.CurrentDirectory, "Plugins.Infrastructure.Tests.Configuration"));
+        
 
         protected void SetupFederatedAuthenticationConfiguration(string configFileName)
         {
-            federatedAuthenticationConfiguration = configSectionReader.GetConfigSection<FederatedAuthenticationConfiguration>(configFileName, "federatedAuthentication");
+            federatedAuthenticationConfiguration = configSectionReader.GetConfigSection<FederatedAuthenticationConfiguration>(configFileName,
+                "federatedAuthentication");
         }
     }
 }
