87695f79d7528a494076102aec9d749ef2e71078
johannesvaltonen <johannes.valtonen@affecto.com>
2016-06-16 15:20:41 +0300

    Moved AuthenticationServer.Claims and AuthenticationServer.Plugins.Infrastructure to separate repositories.

--- Source/AuthenticationServer.Plugins.Infrastructure/Configuration/IFederatedAuthenticationConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Affecto.AuthenticationServer.Plugins.Infrastructure.Configuration
-{
-    public interface IFederatedAuthenticationConfiguration
-    {
-        string UserAccountNameClaim { get; set; }
-        string UserDisplayNameClaim { get; set; }
-        string GroupsClaim { get; set; }
-    }
-}
